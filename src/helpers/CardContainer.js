e9a738831d1689e548f248ee0a0d9595f45a1ee7
Sarthak Agarwal <95269183+CARLO-IITI@users.noreply.github.com>
2023-06-21 16:39:27 +0530

    initiatives-pics added

--- src/helpers/CardContainer.js
+++ src/helpers/CardContainer.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 // import "../css/style.css"
 
 const CardContainer = ({ title, body,imagePath}) => (
-  <div className="card" style={{ backgroundImage: "url(" + {imagePath} + ")", }}>
+  <div className="card" style={{ backgroundImage: `url(${imagePath})`, }}>
     <div className="card-content">
       <h2 className="inline-block text-lg font-medium font-bold leading-8 card-title font-montserrat">
         {title}
