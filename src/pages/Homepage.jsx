8173119b94de99200c4963b6b716664805828f91
Garima Upadhyay <garimatg100@gmail.com>
2023-06-26 20:03:47 +0530

    added blogs

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -29,7 +29,7 @@ export default function Homepage(){
         <Gallery/>
         <Gallery2
         galleryImages={galleryImages}/>
-        {/* <Blogs {...BlogsData} /> */}
+        <Blogs {...BlogsData} />
         </>
     )
 }
