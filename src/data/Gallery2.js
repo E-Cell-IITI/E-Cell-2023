3b47fb9c5482b2bf9201e655e07ed4cf98f970d2
Shashant Kumar <shashant.sah@gmail.com>
2023-06-29 18:44:48 +0530

    gallery image is separated

--- src/data/Gallery2.js
+++ src/data/Gallery2.js
@@ -1,39 +1,39 @@
 const galleryImages = [
     {
-      img: '/images/AMIT1391.jpeg'
+      img: '/images/Gallery2/AMIT1391.jpeg'
     },
     {
-      img: "/images/AMIT1558.jpeg"
+      img: "/images/Gallery2/AMIT1558.jpeg"
     },
     {
-       img: "/images/AMIT1387.jpeg"
+       img: "/images/Gallery2/AMIT1387.jpeg"
      },
      {
-       img: "images/IMG_0238.jpeg"
+       img: "images/Gallery2/IMG_0238.jpeg"
     },
     {
-      img: '/images/AMIT1395.jpeg'
+      img: '/images/Gallery2/AMIT1395.jpeg'
     },
      {
-       img: "/images/AMIT1455.jpeg"
+       img: "/images/Gallery2/AMIT1455.jpeg"
      },
      {
-         img: "images/IMG_0465.jpeg"
+         img: "images/Gallery2/IMG_0465.jpeg"
        },
        {
-        img: "/images/9K6A4248.jpeg"
+        img: "/images/Gallery2/9K6A4248.jpeg"
        },
        {
-        img: "/images/IMG_0425.jpeg"
+        img: "/images/Gallery2/IMG_0425.jpeg"
        },
        {
-        img: "/images/IMG_0320.jpeg"
+        img: "/images/Gallery2/IMG_0320.jpeg"
        },
       {
-         img: "/images/DSC09470.jpeg"
+         img: "/images/Gallery2/DSC09470.jpeg"
        },
       {
-         img: "images/DSC09334.jpeg"
+         img: "images/Gallery2/DSC09334.jpeg"
    }
   ]
   export default galleryImages;
