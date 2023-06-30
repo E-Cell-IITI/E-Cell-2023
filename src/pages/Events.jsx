cf8b2bbcb2afd7ee3f2c504815659feb5cc4a296
aditiganvir28 <aditi10328@gmail.com>
2023-06-30 11:03:45 +0530

    recent and upcoming events done

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -19,10 +19,10 @@ function App(props) {
 
 
     return (
-        <div>
+        <div className="py-16 md:py-24">
 
-            <div className="flex flex-col justify-center items-center px-4 pb-28">
-                <h1 className="text-center pt-10 pb-28 text-6xl font-semibold">Recent <span className="text-[#0060a1]">Events</span></h1>
+            <div className="flex flex-col justify-center items-center px-4 pb-20">
+                <h1 className="text-center pt-10 pb-20 md:pb-28 text-6xl font-semibold">Recent <span className="text-[#0060a1]">Events</span></h1>
 
                 <div className="flex justify-center items-center w-full lg:px-12">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-4 md:px-4 ">
@@ -43,10 +43,10 @@ function App(props) {
 
 
                                     <div className="w-full absolute inset-0 flex flex-col items-center justify-center text-center translate-y-[64%] group-hover:translate-y-0 transition-transform ease-in duration-700 group-hover:bg-[#1B2A2C] group-hover:bg-opacity-75">
-                                        <div className="bg-[#1B2A2C] bg-opacity-75 w-full group-hover:bg-opacity-0 py-5">
+                                        <div className="bg-[#1B2A2C] bg-opacity-75 w-full group-hover:bg-opacity-0 py-7 pb-9">
                                             <h1 className="font-dmserif text-3xl font-bold text-white group-hover:mb-4 ">{val.heading}</h1>
                                         </div>
-                                        <p className="text-lg italic text-white mb-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300">{val.content}</p>
+                                        <p className="text-lg italic text-white mb-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 h-[19vh] md:h-[13vh] lg:h-[16vh]">{val.content}</p>
 
                                         <div className="class"><button onClick={display_info}>Know More...</button></div>
 
@@ -60,10 +60,10 @@ function App(props) {
 
 
             <div className="flex flex-col justify-center items-center">
-                <h1 className="text-center py-16 text-6xl font-semibold wow animate__animated animate__fadeInUpBig">UPCOMING <span className="text-[#0060a1]">EVENTS</span></h1>
+                <h1 className="text-center py-20 text-6xl font-semibold wow animate__animated animate__fadeInUpBig">UPCOMING <span className="text-[#0060a1]">EVENTS</span></h1>
 
                 <div className="w-full">
-                    <div className="grid  pb-32 ">
+                    <div className="grid  pb-20 md:pb-32">
                         {props.upcoming.map((val, e) => (
                             <div className="group relative items-center justify-center overflow-hidden cursor-pointer hover:shadow-xl hover:shadow-black/30">
                                 <div className=" h-full lg:h-[75vh]">
