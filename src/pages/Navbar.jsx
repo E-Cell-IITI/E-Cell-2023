dc40276192ac93afde1a74eabaf502e7b8853fca
devanshi00 <117909426+devanshi00@users.noreply.github.com>
2023-07-08 18:46:41 +0530

    Update Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,17 +1,19 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState, useRef } from "react";
 import "../css/Navbar.css";
 import { Link } from "react-scroll";
 import { useLocation } from "react-router-dom";
-export default function Header(props) {
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
-  }
 
+export default function Header(props) {
   const location = useLocation();
   const currentPath = location.pathname;
 
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [navbar, setNavbar] = useState(false);
+  const [activeSection, setActiveSection] = useState(null);
+  const[ colorChange,setColorchange]=useState(false)
+  const navbarRef = useRef(null);
+  const sections = ["home", "about", "initiatives", "events", "team", "gallery", "blogs", "contact"];
+
   const changeBackground = () => {
     if (window.scrollY >= 1) {
       setNavbar(true);
@@ -19,44 +21,68 @@ export default function Header(props) {
       setNavbar(false);
     }
   };
-  window.addEventListener("scroll", changeBackground);
 
-  const [colorChange, setColorchange] = useState(false);
-  const [textColor, setTextColor] = useState(false);
-
-  useEffect(()=>{
-    if(currentPath=='/team' || currentPath=='/gallery'){
-      setColorchange(true);
-    }
-  },[])
-
-  
   const changeNavbarColor = () => {
-    if(currentPath=='/team' || currentPath=='/gallery'){
-      setColorchange(true);
-    }
-    else if (window.scrollY >= 200) {
+    if (window.scrollY >= 200 || currentPath === "/team" || currentPath === "/gallery") {
       setColorchange(true);
     } else {
       setColorchange(false);
     }
   };
-  window.addEventListener("scroll", changeNavbarColor);
 
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
 
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavbarColor);
+    return () => {
+      window.removeEventListener("scroll", changeNavbarColor);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleSectionChange = () => {
+      const scrollPosition = window.scrollY + navbarRef.current.offsetHeight;
+      const windowHeight = window.innerHeight;
+
+      for (let i = 0; i < sections.length; i++) {
+        const section = document.getElementById(sections[i]);
+        if (section) {
+          const { top, bottom } = section.getBoundingClientRect();
+          const sectionTop = top + window.scrollY;
+          const sectionBottom = bottom + window.scrollY;
+
+          if (sectionTop <= scrollPosition && scrollPosition < sectionBottom) {
+            setActiveSection(sections[i]);
+            break;
+          }
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleSectionChange);
+    return () => {
+      window.removeEventListener("scroll", handleSectionChange);
+    };
+  }, []);
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   return (
     <div
+      ref={navbarRef}
       className={classNames(
         "nav flex fixed items-center justify-between py-1 bg-transparent top-0 z-50 w-full px-8 lg:px-16 right-0 left-0",
         colorChange ? "bg-white opacity-75" : "bg-transparent"
       )}
     >
-      <img
-        src="./ecell.png"
-        alt="Logo"
-        className="object-contain w-32 mx-6 md:mx-4"
-      />
+      <img src="./ecell.png" alt="Logo" className="object-contain w-32 mx-6 md:mx-4" />
 
       <nav>
         <section className="MOBILE-MENU flex lg:hidden">
@@ -89,23 +115,28 @@ export default function Header(props) {
             <ul className="flex flex-col items-center justify-between min-h-[250px] text-[#0060A1] overflow-y-auto mobile-menu">
               {props.links.map((val, index) => (
                 <li
-                  className="border-b border-slate-100 my-8 uppercase font-semibold text-slate-100 text-lg mobile"
+                  className=
+                    "border-b border-slate-100 my-8 uppercase font-semibold text-slate-100 text-lg mobile"
+                    
+                  
                   key={index}
                 >
                   {val.title === "TEAM" || val.title === "GALLERY" ? (
-                <a href={val.link} onClick={() => setIsNavOpen((prev) => !prev)}>{val.title}</a>
-              ) : (
-                <Link
-                  to={val.link}
-                  spy={true}
-                  smooth={true}
-                  offset={-70} // Adjust this offset if necessary
-                  duration={500} // Adjust the scrolling duration if necessary
-                  onClick={() => setIsNavOpen((prev) => !prev)}
-                >
-                  {val.title}
-                </Link>
-              )}
+                    <a href={val.link} onClick={() => setIsNavOpen((prev) => !prev)}>
+                      {val.title}
+                    </a>
+                  ) : (
+                    <Link
+                      to={val.link}
+                      spy={true}
+                      smooth={true}
+                      offset={-70} // Adjust this offset if necessary
+                      duration={500} // Adjust the scrolling duration if necessary
+                      onClick={() => setIsNavOpen((prev) => !prev)}
+                    >
+                      {val.title}
+                    </Link>
+                  )}
                 </li>
               ))}
             </ul>
@@ -113,13 +144,15 @@ export default function Header(props) {
         </section>
 
         <ul className="DESKTOP-MENU hidden space-x-12 lg:flex text-white">
-
           {props.links.map((val, index) => (
             <li
-              className="text-xl font-semibold text-stone-950 desktop"
+              className={classNames(
+                "text-xl font-semibold text-stone-950 desktop",
+                activeSection === val.title.toLowerCase() ? "active" : ""
+              )}
               key={index}
             >
-              {val.title === "TEAM" || val.title === "GALLERY" || val.title==="HOME"? (
+              {val.title === "TEAM" || val.title === "GALLERY" || val.title === "HOME" ? (
                 <a href={val.link}>{val.title}</a>
               ) : (
                 <Link
@@ -136,28 +169,34 @@ export default function Header(props) {
           ))}
         </ul>
       </nav>
+
       <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: rgb(17 24 39);
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-     
-
-
-    `}</style>
+        .hideMenuNav {
+          display: none;
+        }
+        .showMenuNav {
+          display: block;
+          position: absolute;
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background: rgb(17 24 39);
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+        .active{
+          transform: scale(1.2); /* Apply scaling effect */
+        }
+       
+        
+        }
+      `}</style>
     </div>
   );
 }
+
+
